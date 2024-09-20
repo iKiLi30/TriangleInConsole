5ccf422eacb2645cae84305518ddcd81f66c57d2
WIN-HIJ4FH2GJF8\user <jolahmilovski@gmail.com>
2024-09-20 13:25:52 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace TriangleInConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a heigth of a triangle: ");
+            int heigth = int.Parse(Console.ReadLine());
+            for (int i = 0; i < heigth; i++)
+            {
+                for (int j = heigth; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j <= i; j++)
+                {
+
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < heigth; i++)
+            {
+                for (int j = heigth; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+
+                for (int j = 0; j <= i; j++)
+                {
+
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+
+            }
+                       
+        }
+    }
+}
